166552109298b2a49c92ad8ee94cffd2e7bc7811
Reio Kruusement <reiokr@gmail.com>
2020-04-22 22:36:48 +0300

    v 1.2

--- app.js
+++ app.js
@@ -28,9 +28,22 @@ function showLoader() {
     loader.classList.add("show-loader"), addBtn.classList.add("hide-floater"), input.classList.add("hide-floater"), yt.classList.add('hide-floater')
 }
 
+function showLoader1(id) {
+    const loader1 = document.getElementById("loader" + id);
+    const card = document.getElementById(id).firstElementChild;
+    card.style.visibility = "hidden";
+    loader1.classList.add("show-loader");
+}
+
 // hide loader image
 function removeLoader() {
     loader.classList.remove("show-loader"), addBtn.classList.remove("hide-floater"), input.classList.remove("hide-floater"), yt.classList.remove('hide-floater')
+}
+
+function removeLoader1(id) {
+    const card = document.getElementById(id).firstElementChild;
+    card.style.visibility = "visible";
+    loader1.classList.remove("show-loader");
 }
 
 // show error message
@@ -61,39 +74,57 @@ var toHHMMSS = (secs) => {
         .join(":")
 }
 
-// create new bookmark
-function showBookmark(e) {
-    e.preventDefault();
-    //check if input value is empty string
-    if (!input.value) {
-        errorMsg('Web URL Required!');
-        return;
-    }
-    showLoader();
-
-    let bookmark = input.value;
-
+// validate url with regular expressions
+function checkBookmark(bookmark) {
     // check if input has http:// and if not then append
     /^https?:\/\//i.test(bookmark) || (bookmark = `http://${bookmark}`);
-
     // check if youtube video ends ?t=[digits] or &t=[digits]
     re = /[\?&]t=[0-9]{1,}$/;
     let urlEnd;
     let videoStart;
     if (!re.test(bookmark)) {
-        console.log(`${bookmark} does not contain ${re.source}`);
         urlEnd = "";
         videoStart = "";
     } else {
-        console.log(`${bookmark} contains ${re.source}`);
-        urlEnd = re.exec(bookmark)[0];
-        let time = toHHMMSS(urlEnd.replace(/[\?&]t=/, ''));
-        videoStart = `<br><p class="video-start">Video start at ${time}</p>`;
+        urlEnd = re.exec(bookmark)[0]; // if youtube url ends with video start time .. add to url end 
+        let time = toHHMMSS(urlEnd.replace(/[\?&]t=/, '')); // convert to secons minutes and hours
+        videoStart = `<br><p class="video-start">Video start at ${time}</p>`; // create info text in screen
     };
+    return [bookmark, urlEnd, videoStart];
+}
+// check if url is from YouTube and make some changes with regular expressions
+function checkYouTubeUrl(url) {
+    // check if input is YouTube video and replace www.youtube.com/watch?v= to youtu.be/
+    let youtubeUrl = url;
+    let yt = /www\.youtube\.com\/watch\?v=/i;
+    let newUrl;
+    if (!yt.test(youtubeUrl)) {} else {
+        let newString = yt.exec(youtubeUrl);
+        newUrl = youtubeUrl.replace(newString[0], 'youtu.be/');
+        url = newUrl;
+    }
+    // check if in youtube url ?list has changed to &list and change it back
+    let youtubeUrlnew = url
+    let andUrl;
+    let and = /&list/;
+    if (!and.test(youtubeUrlnew)) {
+        // console.log('does not contain &list')
+    } else {
+        let newString = and.exec(youtubeUrlnew);
+        andUrl = youtubeUrlnew.replace(newString[0], '?list');
+        // console.log(`${youtubeUrlnew} contains ${and.source}`)
+        url = andUrl;
+    }
+    return url
+}
 
-    const url = encodeURIComponent(bookmark);
+// fetch url info from https://opengraph.io/api/1.1/site
+function getUrl(url, bookmark) {
+    // get variables from checkBookmark
+    let urlEnd = checkBookmark(bookmark)[1];
+    let videoStart = checkBookmark(bookmark)[2];
 
-    // fetch site info from https://opengraph.io/api/1.1/site
+    // fetch url
     fetch(`${apiUrl}/${url}?app_id=${appId}`)
         .then(response => response.json())
         .then(data => {
@@ -102,32 +133,14 @@ function showBookmark(e) {
                 image: data.hybridGraph.image,
                 url: data.hybridGraph.url + urlEnd
             };
-            // check if input is YouTube video and replace www.youtube.com/watch?v= to youtu.be/
-            let youtubeUrl = bookmark.url;
-            let yt = /www\.youtube\.com\/watch\?v=/i;
-            let newUrl;
-            if (!yt.test(youtubeUrl)) {} else {
-                let newString = yt.exec(youtubeUrl);
-                newUrl = youtubeUrl.replace(newString[0], 'youtu.be/');
-                bookmark.url = newUrl;
-            }
-
-            // check if in youtube url ?list has changed to &list and change it back
-            let youtubeUrlnew = bookmark.url
-            let andUrl;
-            let and = /&list/;
-            if (!and.test(youtubeUrlnew)) {
-                // console.log('does not contain &list')
-            } else {
-                let newString = and.exec(youtubeUrlnew);
-                andUrl = youtubeUrlnew.replace(newString[0], '?list');
-                // console.log(`${youtubeUrlnew} contains ${and.source}`)
-                bookmark.url = andUrl;
-            }
+            // check url
+            newUrl = checkYouTubeUrl(bookmark.url);
+            bookmark.url = newUrl;
 
             // add bookmark to the list
             bookmarks.push(bookmark);
             // show bookmarks on the screen
+
             fillBookmarksList(bookmarks);
             // store bookmarks in local storage
             storeBookmarks(bookmarks);
@@ -135,22 +148,75 @@ function showBookmark(e) {
             bookmarkForm.reset();
             // remove loader image
             removeLoader();
-            // handle fetch error
+            // handle errors
         }).catch(error => {
             console.log(error);
             errorMsg("We can't find what you are looking!");
             removeLoader();
             bookmarkForm.reset();
         });
+}
+
+function getEditedUrl(url, id, bookmark) {
+    // get variables from checkBookmark
+    let urlEnd = checkBookmark(bookmark)[1];
+    let videoStart = checkBookmark(bookmark)[2];
+
+    // fetch url
+    fetch(`${apiUrl}/${url}?app_id=${appId}`)
+        .then(response => response.json())
+        .then(data => {
+            const bookmark = {
+                title: data.hybridGraph.title + videoStart,
+                image: data.hybridGraph.image,
+                url: data.hybridGraph.url + urlEnd
+            };
+            newUrl = checkYouTubeUrl(bookmark.url);
+            bookmark.url = newUrl;
+            fillBookmark(bookmark, id);
+            bookmarks[id] = bookmark;
+            storeBookmarks(bookmarks);
+            // removeLoader1();
+        })
+        .catch(error => {
+            console.log(error);
+            removeLoader1(id);
+            bookmarkForm.reset();
+        })
+}
+
+// create new bookmark
+function showBookmark(e) {
+    e.preventDefault();
+    // bookmark from input field
+    let bookmark = input.value;
+    //check if input value is empty string
+    if (!input.value) {
+        errorMsg('Web URL Required!');
+        return;
+    }
+    // show loading animation
+    showLoader();
+    // validate bookmark with checkBookmark function
+    bookmark = checkBookmark(bookmark)[0];
+    const url = encodeURIComponent(bookmark);
+    // function for scraping url using https://opengraph.io  API
+    getUrl(url, bookmark);
     // unzoom floater
     closeFloater();
 }
 
 // fill the screen with bookmarks using map method. map parameters are bookmark and id for close icon
 function fillBookmarksList(bookmarks = []) {
-    const bookmarksHtml = bookmarks.map((bookmark, id) => `<div class="card"><a class="bookmark" href="${bookmark.url}" target="_blank">\n        <img class = "img" src="${bookmark.image}">\n        <div class = "title">${bookmark.title}</div>\n        </a><div class="edit-input"><form><input type="text" value="${bookmark.url}"><button type="submit" class="edit-icon-ok" data-id="${id}">&#10004;</button></form><span class="edit-icon-close" data-id="${id}">&#10006;</span></div><div class="close tooltip"><span class="icon" data-id="${id}">&#128465;</span><span class="tooltiptext">Remove Bookmark?</span></div><div class="edit tooltip"><span class="edit-icon" data-id="${id}">&#128397;</span><span class="tooltipedit">Edit Bookmark?</span></div></div>`).join(" ");
+    const bookmarksHtml = bookmarks.map((bookmark, id) => `<div class="card" id="${id}"><a class="bookmark" href="${bookmark.url}" target="_blank">\n        <img class = "img" src="${bookmark.image}">\n        <div class = "title">${bookmark.title}</div>\n        </a><div class="loader1" id="loader${id}"></div><div class="edit-input"><form><input type="text" value="${bookmark.url}"><button type="submit" class="edit-icon-ok" data-id="${id}">&#10004;</button></form><span class="edit-icon-close" data-id="${id}">&#10006;</span></div><div class="close tooltip"><span class="icon" data-id="${id}">&#128465;</span><span class="tooltiptext">Remove Bookmark?</span></div><div class="edit tooltip"><span class="edit-icon" data-id="${id}">&#128397;</span><span class="tooltipedit">Edit Bookmark?</span></div></div>`).join(" ");
     output.innerHTML = bookmarksHtml;
     lessCards();
+}
+
+// fill edited bookmark
+function fillBookmark(bookmark, id) {
+    const card = document.getElementById(id);
+    card.innerHTML = `<a class="bookmark" href="${bookmark.url}" target="_blank">\n        <img class = "img" src="${bookmark.image}">\n        <div class = "title">${bookmark.title}</div>\n        </a><div class="loader1" id="loader${id}"></div><div class="edit-input"><form><input type="text" value="${bookmark.url}"><button type="submit" class="edit-icon-ok" data-id="${id}">&#10004;</button></form><span class="edit-icon-close" data-id="${id}">&#10006;</span></div><div class="close tooltip"><span class="icon" data-id="${id}">&#128465;</span><span class="tooltiptext">Remove Bookmark?</span></div><div class="edit tooltip"><span class="edit-icon" data-id="${id}">&#128397;</span><span class="tooltipedit">Edit Bookmark?</span></div>`;
 }
 
 // remove bookmark from screen and local storage
@@ -162,7 +228,6 @@ function removeBookmark(e) {
     if (!answer) return;
     // pass the id value to index
     const index = e.target.dataset.id;
-
     // remove bookmark using splice method
     bookmarks.splice(index, 1);
     fillBookmarksList(bookmarks);
@@ -179,36 +244,49 @@ function editBookmark(e) {
         editInput.classList.toggle('show-edit-input');
     }
     const editForm = editInput.firstElementChild;
-    const closeBtn = e.target.parentElement.previousElementSibling.previousElementSibling.firstElementChild.lastElementChild;
-    console.log(closeBtn)
-
+    // add event listener / submit edited bookmark
     editForm.addEventListener('submit', changeBookmark);
-
     e.preventDefault()
 }
 
-// close edit bookmark
+// close bookmark editing field
 function closeEdit(e) {
     if (!e.target.matches('.edit-icon-close')) return;
-    // const editIconClose = e.target;
+    // remove input field
     const editInput = e.target.parentElement;
     editInput.classList.remove('show-edit-input');
-
     e.preventDefault()
 }
 
 // Change bookmark
 function changeBookmark(e) {
     if (!e.target.matches('form')) return;
-    const input = e.target.firstElementChild
-    console.log(input.value);
-    // closeEdit();
+    // get input value
+    let input = e.target.firstElementChild.value;
+    // get id
+    const id = e.target.lastElementChild.dataset.id;
+    // validate url
+    input = checkBookmark(input)[0];
+    const url = encodeURIComponent(input);
+    // show loading animation
+    showLoader1(id);
+    //  function for scraping edited url using https://opengraph.io 
+    getEditedUrl(url, id, input);
+    // remove input field
+    const editInput = e.target.parentElement;
+    editInput.classList.remove('show-edit-input');
     e.preventDefault()
 }
 
 // Store bookmarks in local storage
 function storeBookmarks(bookmarks = []) {
     localStorage.setItem("bookmarks", JSON.stringify(bookmarks))
+}
+
+// less cards in row for bigger screens
+function lessCards() {
+    const countCard = output.childElementCount;
+    countCard <= 36 ? output.classList.add('less-cards') : output.classList.remove('less-cards')
 }
 
 // event listeners
@@ -228,9 +306,3 @@ output.addEventListener('mouseout', (event) => {
         newimg.parentElement.parentElement.style.zIndex = "0"
     }
 });
-
-// less cards in row for bigger screens
-function lessCards() {
-    const countCard = output.childElementCount;
-    countCard <= 36 ? output.classList.add('less-cards') : output.classList.remove('less-cards')
-}
